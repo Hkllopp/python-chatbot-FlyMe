03c65d1b17052ab052061f3e4174c1d771fd22a4
Augustin BAR <baraugustin26@gmail.com>
2022-11-22 12:28:15 +0100

    Added print to init_func

--- app.py
+++ app.py
@@ -93,6 +93,7 @@ async def messages(req: Request) -> Response:
 
 
 def init_func(self):
+    print("init_func")
     APP = web.Application(middlewares=[aiohttp_error_middleware])
     APP.router.add_post("/api/messages", messages)
     return APP
