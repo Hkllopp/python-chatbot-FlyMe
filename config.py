2a659be35cf9906940d562b00e280d54e34cfa11
Augustin BAR <baraugustin26@gmail.com>
2022-12-01 11:44:35 +0100

    Addind others librairies

--- config.py
+++ config.py
@@ -11,3 +11,10 @@ class DefaultConfig:
     PORT = 8000
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    LUIS_APP_ID = os.environ.get("LuisAppId", "")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "")
