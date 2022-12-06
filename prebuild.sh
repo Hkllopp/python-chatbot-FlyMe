6e45f96e7d5c0e09ea4bd0ea2e8968820e725d91
Augustin BAR <baraugustin26@gmail.com>
2022-12-06 18:27:24 +0100

    Revert "updating prebuild.sh with python commands"
    This reverts commit 0499a3b610faa3bf649d47adbf0c62f6032fa781.

--- prebuild.sh
+++ prebuild.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-echo "prebuild.sh: Installing Python dependencies"
-python --version
-python -m venv env
-source env/bin/activate
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+echo "$(ls)"
